4bfb442b90d4d54bea66b1dd7d3f29bf4733aefd
katierose9 <kwhitney98@gmail.com>
2018-03-20 19:19:54 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function theBeatlesPlay(musicians, instruments) {
+  var arr = []
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return arr
+
+}
+
+function johnLennonFacts(facts) {
+  var arr = []
+
+  let i = 0
+  while (i < facts.length) {
+    arr.push(`${facts[i]}!!!`)
+    i++
+  }
+
+  return arr
+}
